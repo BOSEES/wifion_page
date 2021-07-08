bf3cf5102e1656dd71cc453ea61b8aa41cc09f47
bosees <rladuddlf3@naver.com>
2021-07-08 16:07:40 +0900

    서비스 특징 애니메이션 구현 및 포토샵 작업

--- js/index.js
+++ js/index.js
@@ -3,13 +3,17 @@
 const navbar = document.querySelector("#navbar");
 const navbarLists = document.querySelector(".navbar__lists");
 const navbarList = document.querySelector(".navbar__list");
+const serviceFeatures = document.querySelector("#service__features");
+const serviceFeaturesFlag = document.querySelectorAll(".service__features__flag");
+let index = 0;
 
+console.log(serviceFeaturesFlag);
 const navbarMoveScroll = (event) => {
   const location = document.querySelector(`#${event.target.dataset.link}`).offsetTop;
   window.scrollTo({top: location - 110,behavior: "smooth" } );
 };
 
-const navbarRepaint = (event) => {
+const navbarRepaint = () => {
   if (window.pageYOffset > 300) {
     navbar.classList.add("navbar__move");
   } else {
@@ -17,5 +21,13 @@ const navbarRepaint = (event) => {
   }
 }
 
+const changeFeatures = () => {
+  serviceFeaturesFlag[index % 3].classList.remove("on");
+  index = (index + 1) % 3;
+  serviceFeaturesFlag[index].classList.add("on");
+}
+
+
 navbarLists.addEventListener("click", navbarMoveScroll);
 window.addEventListener("scroll", navbarRepaint);
+serviceFeatures.addEventListener("click", changeFeatures);
